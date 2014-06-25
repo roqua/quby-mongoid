dbc2d5055b1d283ce70fafafdef1390e2acd0a8e
Samuel Esposito <s.esposito@roqua.nl>
2014-06-25 12:03:34 +0200

    add answer api integration specs
    Closes #1
    
    LGTM given by: @marten
    
    Squashed commit of the following:
    
    commit 1404bd48a53e34a9f9e3a1f470335ebfe9e1a575
    Author: Samuel Esposito <s.esposito@roqua.nl>
    Date:   Wed Jun 25 12:01:10 2014 +0200
    
        update appraisal gem files
    
    commit cc7940e1e1495283f3a271a2990f259389f05fc5
    Author: Samuel Esposito <s.esposito@roqua.nl>
    Date:   Wed Jun 25 11:58:17 2014 +0200
    
        update quby
    
    commit b3be2a54b9af0d649e5fbd0fc069e77f711e1487
    Author: Samuel Esposito <s.esposito@roqua.nl>
    Date:   Wed Jun 25 11:44:41 2014 +0200
    
        add answer api integration specs

--- spec/quby/answer_repos/mongoid_repo_spec.rb
+++ spec/quby/answer_repos/mongoid_repo_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'quby/answer_repos/specs'
+require 'quby/api/specs/answers_api_specs'
 
 describe Quby::AnswerRepos::MongoidRepo do
-  it_behaves_like "an answer repository"
+  it_behaves_like 'an answer repository'
+  it_behaves_like 'a valid backend for the answers api'
 end
