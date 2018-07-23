e95ffe01ed1a3a7934570f88d90f3ed661c9653f
Frank Blaauw <frank.blaauw@gmail.com>
2018-07-23 16:10:12 +0200

    Updated to mongoid 6

--- Appraisals
+++ Appraisals
@@ -1,16 +1,11 @@
-appraise "mongoid2" do
-  gem 'mongoid', '~> 2.8'
-  gem 'bson_ext'
-end
-
-appraise "mongoid3" do
-  gem 'mongoid', '~> 3.0'
-end
-
 appraise "mongoid4" do
   gem 'mongoid', '~> 4.0.0.beta1'
 end
 
 appraise "mongoid5" do
   gem 'mongoid', '~> 5.0'
+end
+
+appraise "mongoid6" do
+  gem 'mongoid', '~> 6.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,14 @@ Quby.questionnaire_repo = Quby::Questionnaires::Repos::DiskRepo.new(Quby.fixture
 Quby.answer_repo = Quby::Answers::Repos::MongoidRepo.new
 
 require 'mongoid'
-if ::Mongoid::VERSION > '5'
+if ::Mongoid::VERSION > '6'
+  Mongoid.load!(File.expand_path("../../config/mongoid6.yml", __FILE__), :test)
+elsif ::Mongoid::VERSION > '5'
   Mongoid.load!(File.expand_path("../../config/mongoid5.yml", __FILE__), :test)
 elsif ::Mongoid::VERSION > '4'
   Mongoid.load!(File.expand_path("../../config/mongoid4.yml", __FILE__), :test)
-elsif ::Mongoid::VERSION > '3'
-  Mongoid.load!(File.expand_path("../../config/mongoid3.yml", __FILE__), :test)
 else
-  Mongoid.load!(File.expand_path("../../config/mongoid2.yml", __FILE__))
+  raise 'Not supported'
 end
 
 require 'database_cleaner'
